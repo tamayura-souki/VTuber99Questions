732c8e2aa092e8e3df6a12db87cea5e28fbcbeaf
eteeeeeerminal <momentneet@gmail.com>
2020-04-01 18:15:56 +0900

    矢印キーによる移動を、シフトを押しながら矢印キーで移動に変更

--- site/ask.js
+++ site/ask.js
@@ -12,14 +12,24 @@ var askVue = new Vue({
 
 const body_tag = document.querySelector("body");
 body_tag.onkeydown = keyDown;
+body_tag.onkeyup = keyUp;
+var keyFlag = false;
+
+function keyUp(e){
+  if(e.keyCode == 16) keyFlag = false;
+}
 
 function keyDown(e) {
+  if(e.keyCode == 13) return next(false);
+  if(e.keyCode == 16) keyFlag = true;
+  if(!keyFlag) return;
+
   switch (e.keyCode) {
-    case 13: case 39: case 40:
+  case 39: case 40:
     next(false);
     break;
 
-    case 37: case 38:
+  case 37: case 38:
     next(true);
     break;
   }
